bf0b9093cc513257d771eb84445298be7865331c
Ubuntu <sysadmin@Master.j0zhqsc0p5euhjzm3nlrz5n4ve.jx.internal.cloudapp.net>
2020-08-03 06:38:25 +0000

    add file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-#sudo apt-get update 
+sudo apt-get update 
 #sudo apt-get install git 
 sudo apt-get install openmpi-bin openmpi-common openssh-client openssh-server libopenmpi2 libopenmpi-dev
 #git clone 
